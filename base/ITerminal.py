c34f70c39eade2033159a740d99f8aee155f2ff1
KinitoSs <66869636+KinitoSs@users.noreply.github.com>
2022-11-11 09:25:50 +0300

    fix

--- base/ITerminal.py
+++ base/ITerminal.py
@@ -9,14 +9,12 @@ class ITerminal(metaclass=abc.ABCMeta):
         clear(): "очищает" консоль (на самом деле принтует 80 пустых строк)
         pause(): "нажмите любую кнопку, чтобы продолжить"
     """
-
-    @abc.abstractmethod
+    
     def clear(self) -> None:
         """ "очищает" консоль (на самом деле принтует 80 пустых строк)"""
         print("\n" * 80)
         # pass
 
-    @abc.abstractmethod
     def pause(self) -> None:
         """ "нажмите любую кнопку, чтобы продолжить" """
         os.system("pause")
